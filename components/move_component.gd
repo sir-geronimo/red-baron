d9b3620d29dbe3758434c1938d0e29c0dfb4807c
Enger Jiménez <ejimenezr21@gmail.com>
2024-01-14 13:14:42 -0400

    Nerf enemies

--- components/move_component.gd
+++ components/move_component.gd
@@ -6,6 +6,7 @@ extends Node
 #@export_group("Statistics")
 @export var speed: float = 100.0
 @export var velocity: Vector2 = Vector2.ZERO
+@export_range(0.0, 1.0) var acceleration: float = 0.25
 
 func _physics_process(delta: float) -> void:
 	actor.position += velocity * speed * delta
